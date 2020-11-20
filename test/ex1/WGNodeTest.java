dbc9457b793ddbc34f75d11c4177c4ff61a0fe85
myimac <noiagendl@hotmail.com>
2020-11-20 09:11:44 +0200

    Initial commit

---
+++ test/ex1/WGNodeTest.java
@@ -0,0 +1,22 @@
+package ex1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WGNodeTest {
+
+    @org.junit.jupiter.api.Test
+    void addNi() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeNi() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+}
